42479588d9d05e5a8664e1af844022d4a3a01797
Ariful Islam <hello.mrariful@gmail.com>
2023-11-26 00:10:57 +0600

    news delete API added

--- index.js
+++ index.js
@@ -138,6 +138,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete("/newses/:id", async(req, res) =>{
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await newsCollection.deleteOne(query)
+            res.send(result)
+        })
+
 
 
 
